bea325767a3e349fb293042101469a1b34934451
Marcio de Jesus <marciodejesusrj@gmail.com>
2021-04-02 00:59:07 -0300

    Contador regressivo agora está no game, o widget de intialTimer recebe o novo time a cada iteração.
    Agora os FlipCards são carregados sempre com back() invertido

--- lib/components/initial_timer.dart
+++ lib/components/initial_timer.dart
@@ -1,50 +1,17 @@
-// ignore: must_be_immutable
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
-class InitialTimer extends StatefulWidget {
+class InitialTimer extends StatelessWidget {
+  int _time;
 
-  @override
-  _InitialTimerState createState() => _InitialTimerState();
-}
-
-class _InitialTimerState extends State<InitialTimer> {
-  int _time = 5;
-  Timer _timer;
-
-  void startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (_) {
-      if (this.mounted) {
-        setState(() => _time--);
-      }
-    });
-  }
-
-  void stopTimer() {
-    setState(() => _timer.cancel());
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    startTimer();
-  }
+  InitialTimer(this._time);
 
   @override
   Widget build(BuildContext context) {
     var timeEnded = _time == 0;
-    var text = '$_time';
-
-    if (timeEnded) {
-      stopTimer();
-      text = '';
-    }
 
     return Text(
-      text,
+      timeEnded ? '' : '$_time',
       style: Theme.of(context).textTheme.headline3,
     );
   }
--- lib/screens/memory_game.dart
+++ lib/screens/memory_game.dart
@@ -1,12 +1,49 @@
+import 'dart:async';
+
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/components/initial_timer.dart';
 import 'package:flutter_app/data/cardList.dart';
 import 'package:flutter_app/data/cardModel.dart';
 
-class MemoryGame extends StatelessWidget {
+class MemoryGame extends StatefulWidget {
+  @override
+  _MemoryGameState createState() => _MemoryGameState();
+}
+
+class _MemoryGameState extends State<MemoryGame> {
+  int _time = 5;
+  Timer _timer;
+
+  bool wait() {
+    return _time > 0;
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (_) {
+      if (this.mounted) {
+        setState(() => _time--);
+      }
+    });
+  }
+
+  void stopTimer() {
+    if (!wait()) {
+      setState(() => _timer.cancel());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    startTimer();
+  }
 
   @override
   Widget build(BuildContext context) {
+    stopTimer();
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -14,7 +51,7 @@ class MemoryGame extends StatelessWidget {
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: InitialTimer(),
+                child: InitialTimer(_time),
               ),
               Padding(
                 padding: const EdgeInsets.all(4.0),
@@ -32,14 +69,17 @@ class FlipCardGrid extends StatefulWidget {
   final List<CardModel> _data = getPairsOfCards();
 
   @override
-  _FlipCardGridState createState() => _FlipCardGridState();
+  _FlipCardGridState createState() => _FlipCardGridState(_data);
 }
 
 class _FlipCardGridState extends State<FlipCardGrid> {
+  final List<CardModel> _data;
+
+  _FlipCardGridState(this._data);
 
   @override
   Widget build(BuildContext context) {
-    final _listCard = widget._data;
+    final _listCard = _data;
 
     return GridView.builder(
       shrinkWrap: true,
@@ -49,23 +89,43 @@ class _FlipCardGridState extends State<FlipCardGrid> {
       ),
       itemCount: _listCard.length,
       itemBuilder: (context, index) {
-        return ItemWaitingFinalTime(_listCard[index]);
+        return ItemCard(card: _listCard[index]);
       }
     );
   }
 }
 
 // ignore: must_be_immutable
-class ItemWaitingFinalTime extends StatelessWidget {
-  CardModel _card;
+class ItemCard extends StatelessWidget {
+  CardModel card;
 
-  ItemWaitingFinalTime(this._card);
+  ItemCard({ this.card });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlipCard(
+      key: card.getKey,
+      onFlip: () {}, //temporario
+      flipOnTouch: false, // mudar para ouvir o wait() e o metodo isSelect de card
+      front: Face(card: card), // mudar para ouvir o wait() e o metodo isSelect de card
+      back: Face(), // mudar para ouvir o wait() e o metodo isSelect de card
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class Face extends StatelessWidget {
+  CardModel card;
+
+  Face({ this.card });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(4.0),
-      child: Image.asset(_card.getImageAssetPath),
+      child: Image.asset(
+        card != null ? card.getImageAssetPath : 'assets/question.png'
+      ),
     );
   }
 }
