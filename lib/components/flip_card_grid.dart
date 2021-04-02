e22a739f41cecff780872e345ef11d96392efc84
Marcio de Jesus <marciodejesusrj@gmail.com>
2021-04-02 13:52:14 -0300

    Criado componente flipcard em um arquivo separado. Finalizando o refactor. No fim do jogo, o usuário é levado para o menu principal

---
+++ lib/components/flip_card_grid.dart
@@ -0,0 +1,116 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/data/cardList.dart';
+import 'package:flutter_app/data/cardModel.dart';
+
+class FlipCardGrid extends StatefulWidget {
+  final List<CardModel> _data = getPairsOfCards();
+  bool startGame;
+
+  FlipCardGrid({ this.startGame });
+
+  @override
+  _FlipCardGridState createState() => _FlipCardGridState(_data);
+}
+
+class _FlipCardGridState extends State<FlipCardGrid> {
+  final List<CardModel> _data;
+  CardModel lastCard;
+
+  // Forçar o uso do _data recebido na instância de FlipCard, ou seja, que
+  // ele não seja alterado a cada build.
+  _FlipCardGridState(this._data);
+
+  Widget ItemCard(CardModel currentCard, bool startGame) {
+
+    bool isPair() {
+      return lastCard.getImageAssetPath == currentCard.getImageAssetPath;
+    }
+
+    bool isSameCard() {
+      return lastCard?.getKey == currentCard.getKey;
+    }
+
+    void markThatEqual() {
+      setState(() {
+        currentCard.setIsSelected(true);
+        lastCard.setIsSelected(true);
+        lastCard = null;
+      });
+    }
+
+    void markThatNotEqual() {
+      Future.delayed(const Duration(milliseconds: 1000), () {
+        currentCard.getKey.currentState.toggleCard();
+        lastCard.getKey.currentState.toggleCard();
+        lastCard = null;
+      });
+    }
+
+    return FlipCard(
+      key: currentCard.getKey,
+      onFlipDone: (bool completeFlip) {
+        if (completeFlip) {
+          if (isSameCard()) {
+            lastCard = null;
+          }
+        } else {
+          if (lastCard == null) {
+            lastCard = currentCard;
+          } else {
+            isPair() ? markThatEqual() : markThatNotEqual();
+          }
+        }
+      },
+      flipOnTouch: widget.startGame ? !currentCard.getIsSelected : false,
+      front: widget.startGame ? Face() : Face(card: currentCard),
+      back: !widget.startGame ? Face() : Face(card: currentCard),
+    );
+  }
+
+  void finishGame() {
+    if (_data.every((d) => d.getIsSelected == true)) {
+      print("Won");
+      Future.delayed(const Duration(milliseconds: 160), () {
+        Navigator.pop(context);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _listCard = _data;
+    final _startGame = widget.startGame;
+
+    finishGame();
+
+    return GridView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemCount: _listCard.length,
+        itemBuilder: (context, index) {
+          final card = _listCard[index];
+          return ItemCard(card, _startGame);
+        }
+    );
+  }
+}
+
+class Face extends StatelessWidget {
+  CardModel card;
+
+  Face({ this.card });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(4.0),
+      child: Image.asset(
+          card != null ? card.getImageAssetPath : 'assets/question.png'
+      ),
+    );
+  }
+}
--- lib/screens/memory_game.dart
+++ lib/screens/memory_game.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 
-import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/components/flip_card_grid.dart';
 import 'package:flutter_app/components/initial_timer.dart';
-import 'package:flutter_app/data/cardList.dart';
-import 'package:flutter_app/data/cardModel.dart';
+
+const int TIME_TO_AWAIT = 5;
 
 class MemoryGame extends StatefulWidget {
   @override
@@ -12,7 +12,7 @@ class MemoryGame extends StatefulWidget {
 }
 
 class _MemoryGameState extends State<MemoryGame> {
-  int _time = 5;
+  int _time = TIME_TO_AWAIT;
   Timer _timer;
 
   bool canStartGame() {
@@ -64,78 +64,3 @@ class _MemoryGameState extends State<MemoryGame> {
     );
   }
 }
-
-class FlipCardGrid extends StatefulWidget {
-  final List<CardModel> _data = getPairsOfCards();
-  bool startGame;
-
-  FlipCardGrid({ this.startGame });
-
-  @override
-  _FlipCardGridState createState() => _FlipCardGridState(_data);
-}
-
-class _FlipCardGridState extends State<FlipCardGrid> {
-  final List<CardModel> _data;
-
-  // Forçar o uso do _data recebido na instância de FlipCard, ou seja, que
-  // ele não seja alterado a cada build.
-  _FlipCardGridState(this._data);
-
-  @override
-  Widget build(BuildContext context) {
-    final _listCard = _data;
-    final _startGame = widget.startGame;
-
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-      ),
-      itemCount: _listCard.length,
-      itemBuilder: (context, index) {
-        final card = _listCard[index];
-        return ItemCard(card, _startGame);
-      }
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class ItemCard extends StatelessWidget {
-  CardModel card;
-  bool startGame;
-
-  ItemCard(this.card, this.startGame);
-
-  @override
-  Widget build(BuildContext context) {
-    return FlipCard(
-      key: card.getKey,
-      onFlip: () {}, //temporario
-      flipOnTouch: startGame ? !card.getIsSelected : false,
-      front: startGame ? Face() : Face(card: card),
-      back: !startGame ? Face() : Face(card: card),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class Face extends StatelessWidget {
-  CardModel card;
-
-  Face({ this.card });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(4.0),
-      child: Image.asset(
-        card != null ? card.getImageAssetPath : 'assets/question.png'
-      ),
-    );
-  }
-}
-
-
