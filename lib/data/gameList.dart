cbf615b603ca819d43b934d79b26c2effb6d1c71
Marcio de Jesus <marciodejesusrj@gmail.com>
2021-04-04 14:49:15 -0300

    Resolvido bug da listagem de jogos que não permitia add mais de um jogo.

--- lib/data/gameList.dart
+++ lib/data/gameList.dart
@@ -3,7 +3,7 @@ import 'package:flutter_app/core/routes.dart';
 import 'game_model.dart';
 
 class GameList {
-  static List<GameModel> getGames() {
+  static List<GameModel> getOfflineGames() {
     return [
       GameModel('Jogo da Memória', Routes.memoryGame)
     ];
